7d4e5a19fcf69904983c2427ea43eb3302fd1bea
Mayhem-fok <Reynold.may@georgebrown.ca>
2026-07-22 20:11:36 -0400

    first about me page commit

---
+++ nested-js-file-1.js
@@ -0,0 +1,12 @@
+let htmlTagForButton = '<button></button>';
+let hello = ' hi lol'
+ 
+console.log(' Hey there')
+// ctrl + z for cancelling the last action
+// shift + ctrl + z is for re-doing
+// windows: ctrl+home -> top of file
+// windows: ctrl+end -> bottom of file
+// windows: ctrl+page up -> top of page
+// windows: ctrl+page down -> bottom of page
+// windows: ctrl+backspace -> delete the word to the left of the cursor
+// get to the end of the line, then alt+shift+arrow down it will duplicate the file
